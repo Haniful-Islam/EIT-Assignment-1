7652ce39c28e85c9524ed85bf68673b58526c498
Haniful Islam <haniful360@gmail.com>
2022-11-03 16:06:02 +0600

    every method added

--- array-method.js
+++ array-method.js
@@ -1,5 +1,16 @@
-const conCat1 = 'Bangladesh is a ';
+// concat method
+const conCat1 = 'Bangladesh is a';
 const conCat2 = 'small country'
 
-const result = conCat1.concat(conCat2);//concat method  
-document.write(result);
+const result = conCat1.concat(' ', conCat2);//concat method  
+// document.write(result);
+// console.log(result);
+
+//every method
+const number1 = [20, 56, 30, 59, 34, 24];
+const everyMetod = number1.every(checkout);
+function checkout(num) {
+    return num >= 21;
+}
+// document.write(everyMetod);
+// console.log(everyMethod);
